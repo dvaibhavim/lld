55696f8af00666008b91b1a88871079e5f7e7a97
dvaibhavim <deshpamdevai@gmail.com>
2022-11-03 20:11:56 +0530

    added inheritance in java

---
+++ oops_java/Bird.java
@@ -0,0 +1,30 @@
+// Java program to demonstrate the bird class
+  
+// Implementing the bird class
+public class Bird {
+  
+    // Few properties which
+    // define the bird
+    String color;
+    int legs;
+  
+    // Few operations which the
+    // bird performs
+    public void eat()
+    {
+        System.out.println(
+            "This bird has eaten");
+    }
+  
+    public void fly()
+    {
+        System.out.println(
+            "This bird is flying");
+    }
+}
+
+// Java program to demonstrate the
+// Inheritance
+  
+// Creating the Pigeon class which
+// extends the bird class
---
+++ oops_java/Pigeon.java
@@ -0,0 +1,15 @@
+// Java program to demonstrate the
+// Inheritance
+  
+// Creating the Pigeon class which
+// extends the bird class
+public class Pigeon extends Bird {
+  
+    // Overriding the fly method
+    // which makes this pigeon fly
+    public void fly()
+    {
+        System.out.println(
+            "Pigeon flys!!!!");
+    }
+}
